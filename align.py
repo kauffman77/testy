d29587b30866b8436277fb8a470afbd16d3b110a
Chris Kauffman <kauffman77@gmail.com>
2024-06-02 11:40:38 -0400

    Needleman-Wunsch ready
    See align.py, ready to move it over to the main testyp

--- align.py
+++ align.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
-# from: https://wilkelab.org/classes/SDS348/2019_spring/labs/lab13-solution.html
 
 from difflib import SequenceMatcher
 
-# Use these values to calculate scores
-gap_penalty = -0.0
-match_award = 1
-mismatch_penalty = -1
-
-# Make a score matrix with these two sequences
-seq1 = "ATTACATTTTTTTTTGCGCGCATTA"
-seq2 = "ATGCTTTTAATATTAAGC"
 
 seq1 = [
   "1. Beautiful is better than ugly.",
@@ -37,64 +28,184 @@ seq2 = [
   "cccccccccccccc",
 ]
 
-# A function for making a matrix of zeroes
-def zeros(rows, cols):
-  return [[0]*cols for _ in range(rows)]
+
+def needleman_wunsch(seqA, seqB,
+                     scorefunc="RQR",
+                     score_range=(-0.25,1.0),
+                     gap_cost=-0.1):
+  scorefuncs = {                                              # compare seq elements for scoring
+    "RQR": lambda seqmatcher: seqmatcher.real_quick_ratio(),  # fastest with least accuracy
+    "QR" : lambda seqmatcher: seqmatcher.quick_ratio(),       # fast with more accuracy
+    "R"  : lambda seqmatcher: seqmatcher.ratio(),             # slow with high accuracy
+  }
+  match_score = scorefuncs.get(scorefunc,None)
+  if not match_score:
+    raise Exception("scorefunc '{scorefunc}' is not known, options are {scorefuncs.keys()}")
+
+  score_scale = score_range[1]-score_range[0]
+  score_off   = score_range[0]
+
+  (m,n) = (len(seqA), len(seqB))                     # set up NW score/trace matrix
+  scoremat = [[None]*(n+1) for _ in range(m+1)]      # seqA along rows, seqB along cols
+  for i in range(0, m + 1):                          # initialize matrices for opening gaps
+    scoremat[i][0] = (gap_cost*i,(i,0,"delete",gap_cost))
+  for j in range(0, n + 1):
+    scoremat[0][j] = (gap_cost*j,(0,j,"insert",gap_cost))
+  
+  # MAIN SCORING ALGORITHM
+  for i in range(1, m+1):
+    seqmatcher = SequenceMatcher(None,"",seqA[i-1])  # seqmatcher optimized for stable seq2, vary seq1 more frequentyly
+    for j in range(1, n + 1):
+      if seqA[i-1]==seqB[j-1]:                       # equal elems score a 1.0
+        (tag,raw_score) = ("equal",1.0)
+      else:                                          # unequal elems score in range -0.2 to 1.0
+        seqmatcher.set_seq1(seqB[j-1])               # use seqmatcher for comparison 
+        (tag,raw_score) = ("replace",match_score(seqmatcher))
+      mscore = raw_score*score_scale + score_off                     # scale the score in range
+      scores = ((scoremat[i-1][j-1][0] + mscore,(i-1,j-1,tag,mscore)),         # match seq1/2 elem  
+                (scoremat[i-1][j][0] + gap_cost,(i-1,j,"insert",gap_cost)),  # insert seq2 elem   
+                (scoremat[i][j-1][0] + gap_cost,(i,j-1,"delete",gap_cost)))  # delete seq1 elem   
+      scoremat[i][j] = max(scores)
+  
+  # TRACEBACK COMPUTATION
+  align_score = scoremat[-1][-1][0]              # overall score of the global alignmnet
+  align = []                                     # traceback alignment, elements (seq1,seq2,tag,cost)
+  i,j = m,n
+  while i > 0 or j > 0:
+    (_,info) = scoremat[i][j]
+    align.append(info)
+    i=info[0]
+    j=info[1]
+
+  align.reverse()                                # reverse the alignment in place
+  return (align, align_score)
 
 
-# # A function for determining the score between any two bases in alignment
-# def match_score(alpha, beta):
-#     if alpha == beta:
-#         return match_award
-#     elif alpha == '-' or beta == '-':
-#         raise Exception("WTF^M")
-#     else:
-#         return mismatch_penalty
+def testnw(seq1,seq2):
+  (align,score) = needleman_wunsch(seq1, seq2, scorefunc="RQR")
+  print(f"score: {score:+5.2f} len: {len(align)}")
+  width1 = max([len(seq1[s1])
+                for (s1,s2,tag,_) in align
+                if tag in {"equal","replace","delete"}])
+  width2 = max([len(seq2[s2])
+                for (s1,s2,tag,_) in align
+                if tag in {"equal","replace","insert"}])
+  for s1,s2,tag,cost in align:
+    if tag=="delete":
+      print(f"{seq1[s1]:{width1}} {tag:{8}} {cost:+5.2f} {'':{width2}}")
+    elif tag=="insert":
+      print(f"{'':{width1}} {tag:{8}} {cost:+5.2f} {seq2[s2]:{width2}}")
+    else:
+      print(f"{seq1[s1]:{width1}} {tag:{8}} {cost:+5.2f} {seq2[s2]:{width2}}")
 
-# A function for determining the score between any two bases in alignment
-def match_score(alpha, beta):
-  if alpha == beta:
-    return (1.0,"equal")
-  else:
-    return (SequenceMatcher(None,alpha,beta).quick_ratio(), "replace")
 
 
-def needleman_wunsch(seq1, seq2):
-    
-    # Store length of two sequences
+# has an inverted seq1 len=n / seq2 len=m
+def needleman_wunsch3(seq1, seq2,
+                     scorefunc="RQR",
+                     score_range=(-0.25,1.0),
+                     gap_cost=-0.1):
+  scorefuncs = {                                              # compare seq elements for scoring
+    "RQR": lambda seqmatcher: seqmatcher.real_quick_ratio(),  # fastest with least accuracy
+    "QR" : lambda seqmatcher: seqmatcher.quick_ratio(),       # fast with more accuracy
+    "R"  : lambda seqmatcher: seqmatcher.ratio(),             # slow with high accuracy
+  }
+  match_score = scorefuncs.get(scorefunc,None)
+  if not match_score:
+    raise Exception("scorefunc '{scorefunc}' is not known, options are {scorefuncs.keys()}")
+
+  score_scale = score_range[1]-score_range[0]
+  score_off   = score_range[0]
+
+  (n,m) = (len(seq1), len(seq2))                     # set up NW score/trace matrix
+  scoremat = [[None]*(n+1) for _ in range(m+1)]      # seq1 along columns, seq2 along rows
+  for i in range(0, m + 1):                          # initialize matrices for opening gaps
+    scoremat[i][0] = (gap_cost*i,(i,0,"delete",gap_cost))
+  for j in range(0, n + 1):
+    scoremat[0][j] = (gap_cost*j,(0,j,"insert",gap_cost))
+  
+  # MAIN SCORING ALGORITHM
+  for i in range(1, m + 1):                          
+    seqmatcher = SequenceMatcher(None,"",seq2[i-1])  # seqmatcher optimized for stable seq2, vary seq1 more frequentyly
+    for j in range(1, n + 1):
+      if seq1[j-1]==seq2[i-1]:                       # equal elems score a 1.0
+        (tag,raw_score) = ("equal",1.0)
+      else:                                          # unequal elems score in range -0.2 to 1.0
+        seqmatcher.set_seq1(seq1[j-1])               # use seqmatcher for comparison 
+        (tag,raw_score) = ("replace",match_score(seqmatcher))
+      mscore = raw_score*score_scale + score_off                     # scale the score in range
+      scores = ((scoremat[i-1][j-1][0] + mscore,(i-1,j-1,tag,mscore)),         # match seq1/2 elem  
+                (scoremat[i-1][j][0] + gap_cost,(i-1,j,"insert",gap_cost)),  # insert seq2 elem   
+                (scoremat[i][j-1][0] + gap_cost,(i,j-1,"delete",gap_cost)))  # delete seq1 elem   
+      scoremat[i][j] = max(scores)
+  
+  # TRACEBACK COMPUTATION
+  align_score = scoremat[-1][-1][0]              # overall score of the global alignmnet
+  align = []                                     # traceback alignment, elements (seq1,seq2,tag,cost)
+  i,j = m,n
+  while i > 0 or j > 0:
+    (_,info) = scoremat[i][j]
+    align.append(info)
+    i=info[0]
+    j=info[1]
+
+  align.reverse()                                # reverse the alignment in place
+  return (align, align_score)
+
+def testnw3(seq1,seq2):
+  (align,score) = needleman_wunsch(seq1, seq2, scorefunc="RQR")
+  print(f"score: {score:+5.2f} len: {len(align)}")
+  width1 = max([len(seq1[s1])
+                for (s2,s1,tag,_) in align
+                if tag in {"equal","replace","delete"}])
+  width2 = max([len(seq2[s2])
+                for (s2,s1,tag,_) in align
+                if tag in {"equal","replace","insert"}])
+  for s2,s1,tag,cost in align:
+    if tag=="delete":
+      print(f"{seq1[s1]:{width1}} {tag:{8}} {cost:+5.2f} {'':{width2}}")
+    elif tag=="insert":
+      print(f"{'':{width1}} {tag:{8}} {cost:+5.2f} {seq2[s2]:{width2}}")
+    else:
+      print(f"{seq1[s1]:{width1}} {tag:{8}} {cost:+5.2f} {seq2[s2]:{width2}}")
+
+
+def needleman_wunsch2(seq1, seq2, gap_cost=-0.1, scorefunc="QR"):
+    scorefuncs = {
+      "RQR": lambda seqmatcher: seqmatcher.real_quick_ratio(),
+      "QR" : lambda seqmatcher: seqmatcher.quick_ratio(),
+      "R"  : lambda seqmatcher: seqmatcher.ratio(),
+    }
+    match_score = scorefuncs.get(scorefunc,None)
+    if not match_score:
+      raise Exception("scorefunc '{scorefunc}' is not known, options are {scorefuncs.keys()}")
+
+    # set up NW score/trace matrices
     n = len(seq1)  
     m = len(seq2)
-    
-    # Generate matrix of zeros to store scores
-    score = zeros(m+1, n+1)
-    trace = zeros(m+1, n+1)
+    score = [[None]*(n+1) for _ in range(m+1)]
+    trace = [[None]*(n+1) for _ in range(m+1)]
    
-    # Calculate score table
-    
-    # Fill out first column
+    # initialize matrices for opening gaps
     for i in range(0, m + 1):
-        score[i][0] = gap_penalty * i
+        score[i][0] = gap_cost * i
         trace[i][0] = "delete"
-    
-    # Fill out first row
     for j in range(0, n + 1):
-        score[0][j] = gap_penalty * j
+        score[0][j] = gap_cost * j
         trace[0][j] = "insert"
     
-    # Fill out all other values in the score matrix
     for i in range(1, m + 1):
         seqmatcher = SequenceMatcher(None,"",seq2[i-1])
         for j in range(1, n + 1):
-            (tag,ms) = ("equal",1.0,)
-            if seq1[j-1]!=seq2[i-1]:
-              seqmatcher.set_seq1(seq1[j-1])
-              (tag,ms) = ("replace",seqmatcher.quick_ratio())
-              # (tag,ms) = ("replace",seqmatcher.ratio())
-            # (ms,tag) = match_score(seq1[j-1], seq2[i-1])
-            matchs = (score[i-1][j-1] + ms, (tag,ms))
-            delete = (score[i-1][j] + gap_penalty, ("insert",gap_penalty))
-            insert = (score[i][j-1] + gap_penalty, ("delete",gap_penalty))
-            (sc,tr) = max(matchs, delete, insert)
+            if seq1[j-1]==seq2[i-1]:             # equal elems score a 1.0
+              (tag,mscore) = ("equal",1.0)
+            else:                                # unequal sequences score
+              seqmatcher.set_seq1(seq1[j-1])     # in range -0.2 to 1.0
+              tag = "replace"                    # using a comparison function
+              mscore = match_score(seqmatcher)*1.25-0.25
+            (sc,tr) = max((score[i-1][j-1] + mscore, (tag,mscore)),         # match seq1/2 elem
+                          (score[i-1][j] + gap_cost, ("insert",gap_cost)),  # insert seq2 elem
+                          (score[i][j-1] + gap_cost, ("delete",gap_cost)))  # delete seq1 elem
             score[i][j] = sc
             trace[i][j] = tr
     
@@ -104,6 +215,9 @@ def needleman_wunsch(seq1, seq2):
     diff = []   
     i = m
     j = n
+    # for i,r in enumerate(trace):
+    #   for j,c in enumerate(r):
+    #     print(f"({i:{2}},{j:{2}}) {c}")
     while i > 0 or j > 0:
         (tag,scr) = trace[i][j]
         diff.append((tag,scr))
@@ -123,29 +237,6 @@ def needleman_wunsch(seq1, seq2):
         else:
             raise Exception("Problem in traceback")
 
-    # # We'll use i and j to keep track of where we are in the matrix, just like above
-    # while i > 0 and j > 0: # end touching the top or the left edge
-    #     score_current = score[i][j]
-    #     score_diagonal = score[i-1][j-1]
-    #     score_up = score[i][j-1]
-    #     score_left = score[i-1][j]
-        
-    #     # Check to figure out which cell the current score was calculated from,
-    #     # then update i and j to correspond to that cell.
-    #     if score_current == score_diagonal + match_score(seq1[j-1], seq2[i-1]):
-    #         align1.append(seq1[j-1])
-    #         align2.append(seq2[i-1])
-    #         i -= 1
-    #         j -= 1
-    #     elif score_current == score_up + gap_penalty:
-    #         align1.append(seq1[j-1])
-    #         align2.append('-')
-    #         j -= 1
-    #     elif score_current == score_left + gap_penalty:
-    #         align1.append('-')
-    #         align2.append(seq2[i-1])
-    #         i -= 1
-
     # Finish tracing up to the top left cell
     while j > 0:
         align1 += seq1[j-1]
@@ -164,13 +255,16 @@ def needleman_wunsch(seq1, seq2):
     
     return(align1, align2, score[-1][-1], diff)
 
-def testnw(seq1,seq2):
-  (align1,align2,score,diff) = needleman_wunsch(seq1, seq2)
+def testnw2(seq1,seq2):
+  (align1,align2,score,diff) = needleman_wunsch2(seq1, seq2, scorefunc="QR")
   print(f"score: {score} lens: {len(align1)} {len(align2)} {len(diff)}")
 
   width1 = max([len(x) for x in align1])
   for s1,s2,(dt,ds) in zip(align1,align2,diff):
     print(f"{s1:{width1}} {dt:{8}} {ds:+5.2f} {s2}")
+
+  
+
 
 ################################################################################
 # Main Entry point
